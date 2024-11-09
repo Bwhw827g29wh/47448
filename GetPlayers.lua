85932b5ce7b65a419b198eb89e4fb60c680483d5
Bwhw827g29wh <140915245+Bwhw827g29wh@users.noreply.github.com>
2024-11-09 09:11:28 +0800

    Update and rename GetPlayers.txt to GetPlayers.lua

---
+++ GetPlayers.lua
@@ -0,0 +1,40 @@
+local G = {}
+local Players = Game:GetService("Players")
+
+function G:getPlayers(txt)
+    local tl = txt:lower()
+    local found = {}
+
+    if tl == "me" then
+        table.insert(found, Players.LocalPlayer)
+    elseif tl == "random" then
+        local players = Players:GetPlayers()
+        table.insert(found, players[math.random(1, #players)])
+    elseif tl == "others" then
+        for _, v in pairs(Players:GetPlayers()) do
+            if v ~= plr then table.insert(found, v) end
+        end
+    elseif tl == "all" then
+        for _, v in pairs(Players:GetPlayers()) do
+            table.insert(found, v)
+        end
+    elseif tl == "enemies" then
+        for _, v in pairs(Players:GetPlayers()) do
+            if v ~= plr and v.Team ~= plr.Team then table.insert(found, v) end
+        end
+    elseif tl == "team" then
+        for _, v in pairs(Players:GetPlayers()) do
+            if v.Team == plr.Team then table.insert(found, v) end
+        end
+    else
+        for _, v in pairs(Players:GetPlayers()) do
+            if v.Name:lower():match(tl) or v.DisplayName:lower():match(tl) then
+                table.insert(found, v)
+            end
+        end
+    end
+
+    return found
+end
+
+return G
