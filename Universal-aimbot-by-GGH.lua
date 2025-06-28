76f886d5aa4825f1d5023dcc1534f4f578b1a01a
Bwhw827g29wh <140915245+Bwhw827g29wh@users.noreply.github.com>
2025-06-29 04:28:03 +0800

    Update Universal-aimbot-by-GGH.lua

--- Universal-aimbot-by-GGH.lua
+++ Universal-aimbot-by-GGH.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/00a4783c252ddb89b9494c422884ea31"))()
+loadstring(game:HttpGet("https://protected-roblox-scripts.onrender.com/e949efcd791cfa94bfa5a29fbdb85a4b"))()
